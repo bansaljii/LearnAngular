137a1a5961f16e24a8a790177720b05bd57e6a4d
Abhishek Bansal <bansalabhi143368@gmail.com>
2022-05-12 15:40:39 +0530

    for loop in angular

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,21 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
- color="green"
+ fruits=['apple','banana','pine-apple','guava','pomgrenete']
+ info=[
+   {
+     name:'abhishek bansal',
+     email:'bansalabhi143368@gmail.com'
+   },
+   {
+    name:'kartik bhargav',
+    email:'bhragavkartik2000@gmail.com'
+  },{
+    name:'Himanshu saini',
+    email:'saini077@gmail.com'
+  },{
+    name:'Nishant bhargav',
+    email:'bhargavnishant@gmail.com'
+  }
+ ]
 }
